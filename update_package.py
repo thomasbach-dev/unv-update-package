dc20d304d7064b3239f5af73d41930f033f5523e
Thomas Bach <info@thomasbach.dev>
2024-08-19 21:02:12 +0200

    refactor: reduce amount of static code

--- update_package.py
+++ update_package.py
@@ -14,130 +14,133 @@ MACHINE_SETS = {
     "single": ["bsbt1"],  # This is how it is saved in my ssh_config
 }
 
-PARSER = argparse.ArgumentParser(
-    description=" ".join(
-        [
-            "Build and update packages.",
-            "Build packages for each FOLDER, copy them over to the given MACHINES and update them.",
-            "Package buildings is done via Docker.",
-            "Note that packages are only updated, i.e. if they are not installed the package will not",
-            "be installed on the target machine.",
-            'You can skip the check if the package is installed using "-c".',
-            "The script is supposed to run anywhere in a git repository",
-            "and FOLDER has to be relative to the root of the repo.",
-        ]
-    ),
-    epilog="\n".join(
-        [
-            "These are the configured predefined sets of machines:",
-        ]
-        + [f'{key}: {",".join(machines)}' for key, machines in MACHINE_SETS.items()]
-    ),
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-)
-PARSER.add_argument(
-    "folders",
-    metavar="FOLDER",
-    nargs="+",
-    help=" ".join(
-        [
-            'The name of the folders to build (or -if "-b" is given- search) packages for.',
-            "You have to pass in the relative name from the repository root",
-            "Note that more than one package can stem from the same folder.",
-        ]
-    ),
-)
-PARSER.add_argument(
-    "-m",
-    "--machines",
-    default=os.environ.get("UP_MACHINES"),
-    help=" ".join(
-        [
-            "The set of machines to update the packages on.",
-            "This has to be a comma separated list.",
-            'You can also set this argument via the "UP_MACHINES" environment variable.',
-        ]
-    ),
-)
-PARSER.add_argument(
-    "--ssh-config",
-    default=os.environ.get("UP_SSH_CONFIG"),
-    help=" ".join(
-        [
-            "A ssh configuration file to pass on to ssh via the '-F' option.",
-            'You can also set this argument via the "UP_SSH_CONFIG" environment variable.',
-        ]
-    ),
-)
-PARSER.add_argument(
-    "-i",
-    "--docker-image",
-    metavar="IMG",
-    default=os.environ.get("UP_DOCKER_IMAGE"),
-    help=" ".join(
-        [
-            "Use IMG to build packages.",
-            'You can also set this argument via the "UP_DOCKER_IMAGE" environment variable.',
-        ]
-    ),
-)
-PARSER.add_argument(
-    "-p",
-    "--prefix",
-    default="",
-    help=" ".join(
-        [
-            "Prefix all machines with the given string.",
-            "Empty by default.",
-        ]
-    ),
-)
-PARSER.add_argument(
-    "--repository-root",
-    help=" ".join(
-        [
-            "The root of the repository. By default the root is searched by walking up",
-            "the filetree until the `.git' directory is found.",
-        ]
-    ),
-)
-PARSER.add_argument(
-    "-b",
-    "--skip-package-build",
-    help=" ".join(
-        [
-            "Skip the build of the package before distributing it. Note that this results in a",
-            "slightly different behaviour: the `FOLDER's are now simply searched for `.deb' files",
-            "which are then assumed to be distributed. Make sure to clean up built packages!.",
-        ]
-    ),
-    action="store_true",
-)
-PARSER.add_argument(
-    "-c",
-    "--skip-install-check",
-    help="Skip the check if the package is installed on the machines.",
-    action="store_true",
-)
-PARSER.add_argument(
-    "-l",
-    "--log-level",
-    default=logging.INFO,
-    help=" ".join(
-        [
-            "Set the levels of logging which should get printed to stdout. The lower the more",
-            "messages you will see. Default: %(default)s.",
-        ]
-    ),
-    choices=[
-        logging.DEBUG,
-        logging.INFO,
-        logging.WARNING,
-        logging.ERROR,
-        logging.CRITICAL,
-    ],
-    type=int,
-)
+
+def make_arg_parser():
+    parser = argparse.ArgumentParser(
+        description=" ".join(
+            [
+                "Build and update packages.",
+                "Build packages for each FOLDER, copy them over to the given MACHINES and update them.",
+                "Package buildings is done via Docker.",
+                "Note that packages are only updated, i.e. if they are not installed the package will not",
+                "be installed on the target machine.",
+                'You can skip the check if the package is installed using "-c".',
+                "The script is supposed to run anywhere in a git repository",
+                "and FOLDER has to be relative to the root of the repo.",
+            ]
+        ),
+        epilog="\n".join(
+            [
+                "These are the configured predefined sets of machines:",
+            ]
+            + [f'{key}: {",".join(machines)}' for key, machines in MACHINE_SETS.items()]
+        ),
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument(
+        "folders",
+        metavar="FOLDER",
+        nargs="+",
+        help=" ".join(
+            [
+                'The name of the folders to build (or -if "-b" is given- search) packages for.',
+                "You have to pass in the relative name from the repository root",
+                "Note that more than one package can stem from the same folder.",
+            ]
+        ),
+    )
+    parser.add_argument(
+        "-m",
+        "--machines",
+        default=os.environ.get("UP_MACHINES"),
+        help=" ".join(
+            [
+                "The set of machines to update the packages on.",
+                "This has to be a comma separated list.",
+                'You can also set this argument via the "UP_MACHINES" environment variable.',
+            ]
+        ),
+    )
+    parser.add_argument(
+        "--ssh-config",
+        default=os.environ.get("UP_SSH_CONFIG"),
+        help=" ".join(
+            [
+                "A ssh configuration file to pass on to ssh via the '-F' option.",
+                'You can also set this argument via the "UP_SSH_CONFIG" environment variable.',
+            ]
+        ),
+    )
+    parser.add_argument(
+        "-i",
+        "--docker-image",
+        metavar="IMG",
+        default=os.environ.get("UP_DOCKER_IMAGE"),
+        help=" ".join(
+            [
+                "Use IMG to build packages.",
+                'You can also set this argument via the "UP_DOCKER_IMAGE" environment variable.',
+            ]
+        ),
+    )
+    parser.add_argument(
+        "-p",
+        "--prefix",
+        default="",
+        help=" ".join(
+            [
+                "Prefix all machines with the given string.",
+                "Empty by default.",
+            ]
+        ),
+    )
+    parser.add_argument(
+        "--repository-root",
+        help=" ".join(
+            [
+                "The root of the repository. By default the root is searched by walking up",
+                "the filetree until the `.git' directory is found.",
+            ]
+        ),
+    )
+    parser.add_argument(
+        "-b",
+        "--skip-package-build",
+        help=" ".join(
+            [
+                "Skip the build of the package before distributing it. Note that this results in a",
+                "slightly different behaviour: the `FOLDER's are now simply searched for `.deb' files",
+                "which are then assumed to be distributed. Make sure to clean up built packages!.",
+            ]
+        ),
+        action="store_true",
+    )
+    parser.add_argument(
+        "-c",
+        "--skip-install-check",
+        help="Skip the check if the package is installed on the machines.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-l",
+        "--log-level",
+        default=logging.INFO,
+        help=" ".join(
+            [
+                "Set the levels of logging which should get printed to stdout. The lower the more",
+                "messages you will see. Default: %(default)s.",
+            ]
+        ),
+        choices=[
+            logging.DEBUG,
+            logging.INFO,
+            logging.WARNING,
+            logging.ERROR,
+            logging.CRITICAL,
+        ],
+        type=int,
+    )
+    return parser
 
 
 class Configuration(typing.NamedTuple):
@@ -201,7 +204,12 @@ class Configuration(typing.NamedTuple):
         return cls.search_repository_root(current.parent)
 
 
-def main(args: argparse.Namespace):
+def main(argv=sys.argv[1:]):
+    parser = make_arg_parser()
+    args = parser.parse_args()
+    logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+    logger.setLevel(args.log_level)
+
     cfg = Configuration.from_args(args)
     logger.info(f"Configuration is:\n{cfg}")
     pkg_files = build_packages_and_get_paths(cfg)
@@ -359,7 +367,4 @@ def is_package_installed(ssh_ops: SSHOperations, machine: str, package: str) -> 
 
 
 if __name__ == "__main__":
-    args = PARSER.parse_args()
-    logger.addHandler(logging.StreamHandler(stream=sys.stdout))
-    logger.setLevel(args.log_level)
-    main(args)
+    main()
